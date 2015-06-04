ca2ea19719503274f656b62cad7cc6a03a597c9c
Peter Yoachim <yoachim@uw.edu>
2015-06-04 16:07:05 -0700

    remove duplicated code thats now in sims_utils

--- python/lsst/sims/skybrightness/utils.py
+++ python/lsst/sims/skybrightness/utils.py
@@ -9,37 +9,6 @@ def wrapRA(ra):
     ra = ra % (2.0*np.pi)
     return ra
 
-def raDecToAltAz(raRad, decRad, longRad, latRad, mjd):
-    """
-    Numpy-ified the version from  lsst.sims.catalogs.generation.db
-    """
-    lst,last = calcLmstLast(mjd, longRad)
-    haRad = np.radians(last*15.) - raRad
-    altRad = np.arcsin(np.sin(decRad)*np.sin(latRad)+np.cos(decRad)*np.cos(latRad)*np.cos(haRad))
-    argument = (np.sin(decRad) - np.sin(altRad)*np.sin(latRad))/(np.cos(altRad)*np.cos(latRad))
-    # Make sure we don't screw up on machine precision and throw nans.
-    argument[np.where(argument > 1.)] = 1.
-    argument[np.where(argument < -1.)] = -1.
-    azRad = np.arccos(argument)
-    azRad = wrapRA(azRad)
-    rollOver = np.where(np.sin(haRad) >= 0)
-    azRad[rollOver] = 2.*np.pi - azRad[rollOver]
-
-    return altRad, azRad
-
-
-def altAzToRaDec(altRad, azRad, longRad, latRad, mjd):
-    """
-    Numpy-ified the version from  lsst.sims.utils.coordinateTransformations
-    """
-    lst,last = calcLmstLast(mjd, longRad)
-    decRad = np.arcsin(np.sin(latRad)*np.sin(altRad)+ np.cos(latRad)*np.cos(altRad)*np.cos(azRad))
-    haRad = np.arccos((np.sin(altRad) - np.sin(decRad)*np.sin(latRad))/(np.cos(decRad)*np.cos(latRad)))
-    haRad[np.isnan(haRad)] = 0. #catch nans from machine precision roundoff issues going between deg and rad
-    raRad = np.radians(last*15.) - haRad
-    return raRad, decRad
-
-
 def mjd2djd(inDate):
     """
     Convert Modified Julian Date to Dublin Julian Date (what pyephem uses).
