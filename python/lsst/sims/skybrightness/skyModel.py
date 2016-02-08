68f19066eccd93a83958caab22b4a929aea8ef96
Peter Yoachim <yoachim@uw.edu>
2016-02-08 15:54:18 -0800

    flake8

--- python/lsst/sims/skybrightness/skyModel.py
+++ python/lsst/sims/skybrightness/skyModel.py
@@ -1,13 +1,14 @@
 import numpy as np
 import ephem
-from lsst.sims.utils import haversine, _raDecFromAltAz, _altAzPaFromRaDec, Site, ObservationMetaData,calcLmstLast
+from lsst.sims.utils import haversine, _raDecFromAltAz, _altAzPaFromRaDec, Site, ObservationMetaData, calcLmstLast
 import warnings
 from lsst.sims.skybrightness.utils import wrapRA,  mjd2djd
-from .interpComponents import ScatteredStar,Airglow,LowerAtm,UpperAtm,MergedSpec,TwilightInterp,MoonInterp,ZodiacalInterp
+from .interpComponents import ScatteredStar, Airglow, LowerAtm, UpperAtm, MergedSpec, TwilightInterp, MoonInterp, ZodiacalInterp
 from lsst.sims.photUtils import Sed
 
 
 __all__ = ['justReturn', 'stupidFast_RaDec2AltAz', 'stupidFast_altAz2RaDec', 'SkyModel']
+
 
 def justReturn(input):
     """
@@ -15,37 +16,39 @@ def justReturn(input):
     """
     return input
 
-def stupidFast_RaDec2AltAz(ra,dec,lat,lon,mjd):
+
+def stupidFast_RaDec2AltAz(ra, dec, lat, lon, mjd):
     """
     Coordinate transformation is killing performance. Just use simple equations to speed it up
     and ignore abberation, precesion, nutation, nutrition, etc.
     """
-    lmst,last = calcLmstLast(mjd,lon)
-    lmst = lmst/12.*np.pi # convert to rad
+    lmst, last = calcLmstLast(mjd, lon)
+    lmst = lmst/12.*np.pi  # convert to rad
     ha = lmst-ra
     sindec = np.sin(dec)
     sinlat = np.sin(lat)
     coslat = np.cos(lat)
     alt = np.arcsin(sindec*sinlat+np.cos(dec)*coslat*np.cos(ha))
-    az = np.arccos( (sindec-np.sin(alt)*sinlat)/(np.cos(alt)*coslat) )
+    az = np.arccos((sindec-np.sin(alt)*sinlat)/(np.cos(alt)*coslat))
     signflip = np.where(np.sin(ha) > 0)
     az[signflip] = 2.*np.pi-az[signflip]
-    return alt,az
+    return alt, az
 
 
-def stupidFast_altAz2RaDec(alt,az,lat,lon,mjd):
+def stupidFast_altAz2RaDec(alt, az, lat, lon, mjd):
     """
-    Convert alt,az to RA,Dec without taking into account abberation, precesion, diffraction, ect.
+    Convert alt, az to RA, Dec without taking into account abberation, precesion, diffraction, ect.
     """
-    lmst,last = calcLmstLast(mjd,lon)
-    lmst = lmst/12.*np.pi # convert to rad
+    lmst, last = calcLmstLast(mjd, lon)
+    lmst = lmst/12.*np.pi  # convert to rad
 
-    dec = np.arcsin( np.sin(lat)*np.sin(alt) + np.cos(lat)*np.cos(alt)*np.cos(az) )
-    ha = np.arctan2( -np.sin(az)*np.cos(alt), -np.cos(az)*np.sin(lat)*np.cos(alt)+np.sin(alt)*np.cos(lat))
+    dec = np.arcsin(np.sin(lat)*np.sin(alt) + np.cos(lat)*np.cos(alt)*np.cos(az))
+    ha = np.arctan2(-np.sin(az)*np.cos(alt), -np.cos(az)*np.sin(lat)*np.cos(alt)+np.sin(alt)*np.cos(lat))
     ra = (lmst-ha)
     raneg = np.where(ra < 0)
     ra[raneg] = ra[raneg] + 2.*np.pi
-    return ra,dec
+    return ra, dec
+
 
 class SkyModel(object):
 
@@ -57,7 +60,7 @@ class SkyModel(object):
         Instatiate the SkyModel. This loads all the required template spectra/magnitudes
         that will be used for interpolation.
 
-        Observatory: object with attributes lat,lon,elev. But default loads LSST.
+        Observatory: object with attributes lat, lon, elev. But default loads LSST.
         twilight: Include twilight component (True)
         zodiacal: Include zodiacal light component (True)
         moon: Include scattered moonlight compoennt (True)
@@ -67,13 +70,15 @@ class SkyModel(object):
         scatteredStar: Include scattered starlight component (False). This component is part of mergedSpec.
         mergedSpec: Compute the lowerAtm, upperAtm, and scatteredStar simultaneously since they are all
         functions of only airmass (True).
-        mags: (False) By default, the sky model computes a 17,001 element spectrum. If mags is true,
+        mags: (False) By default, the sky model computes a 17, 001 element spectrum. If mags is true,
               the model will return the LSST ugrizy magnitudes.
-        preciceAltAz: False By default, use the fast alt,az to ra,dec coordinate transformations that do not take
-        abberation, diffraction, etc into account. Results in errors up to ~1.5 degrees, but an order of magnitude faster.
+        preciceAltAz: False By default, use the fast alt, az to ra, dec coordinate
+             transformations that do not take
+        abberation, diffraction, etc into account. Results in errors up to ~1.5 degrees,
+            but an order of magnitude faster.
         """
 
-        self.moon=moon
+        self.moon = moon
         self.lowerAtm = lowerAtm
         self.twilight = twilight
         self.zodiacal = zodiacal
@@ -92,9 +97,9 @@ class SkyModel(object):
         else:
             self.npix = 17001
 
-        self.components = {'moon':self.moon, 'lowerAtm':self.lowerAtm, 'twilight':self.twilight,
-                           'upperAtm':self.upperAtm, 'airglow':self.airglow,'zodiacal':self.zodiacal,
-                           'scatteredStar':self.scatteredStar, 'mergedSpec':self.mergedSpec}
+        self.components = {'moon': self.moon, 'lowerAtm': self.lowerAtm, 'twilight': self.twilight,
+                           'upperAtm': self.upperAtm, 'airglow': self.airglow, 'zodiacal': self.zodiacal,
+                           'scatteredStar': self.scatteredStar, 'mergedSpec': self.mergedSpec}
 
         # Check that the merged component isn't being run with other components
         mergedComps = [self.lowerAtm, self.upperAtm, self.scatteredStar]
@@ -102,9 +107,9 @@ class SkyModel(object):
             if comp & self.mergedSpec:
                 warnings.warn("Adding component multiple times to the final output spectra.")
 
-        interpolators = {'scatteredStar':ScatteredStar, 'airglow':Airglow, 'lowerAtm':LowerAtm,
-                         'upperAtm':UpperAtm, 'mergedSpec':MergedSpec, 'moon':MoonInterp,
-                         'zodiacal':ZodiacalInterp, 'twilight':TwilightInterp}
+        interpolators = {'scatteredStar': ScatteredStar, 'airglow': Airglow, 'lowerAtm': LowerAtm,
+                         'upperAtm': UpperAtm, 'mergedSpec': MergedSpec, 'moon': MoonInterp,
+                         'zodiacal': ZodiacalInterp, 'twilight': TwilightInterp}
 
         # Load up the interpolation objects for each component
         self.interpObjs = {}
@@ -114,7 +119,7 @@ class SkyModel(object):
 
         # Set up a pyephem observatory object
         if observatory == 'LSST':
-            self.telescope =  Site('LSST')
+            self.telescope = Site('LSST')
             self.Observatory = ephem.Observer()
             self.Observatory.lat = self.telescope.latitude_rad
             self.Observatory.lon = self.telescope.longitude_rad
@@ -131,7 +136,7 @@ class SkyModel(object):
         """
         Convience function for turning on/off different sky components.
         """
-        self.moon=moon
+        self.moon = moon
         self.lowerAtm = lowerAtm
         self.twilight = twilight
         self.zodiacal = zodiacal
@@ -148,10 +153,9 @@ class SkyModel(object):
         names = ['airmass', 'nightTimes', 'alt', 'az', 'azRelMoon', 'moonSunSep', 'moonAltitude',
                  'altEclip', 'azEclipRelSun', 'sunAlt', 'azRelSun', 'solarFlux']
         types = [float]*len(names)
-        self.points = np.zeros(self.npts, zip(names,types))
+        self.points = np.zeros(self.npts, zip(names, types))
 
-
-    def setRaDecMjd(self,lon,lat,mjd,degrees=False,azAlt=False,solarFlux=130.):
+    def setRaDecMjd(self, lon, lat, mjd, degrees=False, azAlt=False, solarFlux=130.):
         """
         Set the sky parameters by computing the sky conditions on a given MJD and sky location.
 
@@ -159,11 +163,11 @@ class SkyModel(object):
         lat: Latitude-like (Dec or Altitude)
         mjd: Modified Julian Date for the calculation. Must be single number.
         degrees: (False) Assumes lon and lat are radians unless degrees=True
-        azAlt: (False) Assume lon,lat are RA,Dec unless azAlt=True
+        azAlt: (False) Assume lon, lat are RA, Dec unless azAlt=True
         solarFlux: solar flux in SFU Between 50 and 310. Default=130. 1 SFU=10^4 Jy.
         """
         # Wrap in array just in case single points were passed
-        if not type(lon).__module__ == np.__name__ :
+        if not type(lon).__module__ == np.__name__:
             if np.size(lon) == 1:
                 lon = np.array([lon]).ravel()
                 lat = np.array([lat]).ravel()
@@ -183,21 +187,21 @@ class SkyModel(object):
             self.azs = self.ra.copy()
             self.alts = self.dec.copy()
             if self.preciceAltAz:
-                self.ra,self.dec = _raDecFromAltAz(self.alts,self.azs,
-                                                   ObservationMetaData(mjd=self.mjd,site=self.telescope))
+                self.ra, self.dec = _raDecFromAltAz(self.alts, self.azs,
+                                                    ObservationMetaData(mjd=self.mjd, site=self.telescope))
             else:
-                self.ra,self.dec = stupidFast_altAz2RaDec(self.alts,self.azs,
-                                                          self.telescope.latitude_rad,
-                                                          self.telescope.longitude_rad,mjd)
+                self.ra, self.dec = stupidFast_altAz2RaDec(self.alts, self.azs,
+                                                           self.telescope.latitude_rad,
+                                                           self.telescope.longitude_rad, mjd)
         else:
             if self.preciceAltAz:
-                self.alts,self.azs,pa = _altAzPaFromRaDec(self.ra, self.dec,
-                                                          ObservationMetaData(mjd=self.mjd,
-                                                                              site=self.telescope))
+                self.alts, self.azs, pa = _altAzPaFromRaDec(self.ra, self.dec,
+                                                            ObservationMetaData(mjd=self.mjd,
+                                                                                site=self.telescope))
             else:
-                self.alts,self.azs = stupidFast_RaDec2AltAz(self.ra,self.dec,
-                                                            self.telescope.latitude_rad,
-                                                            self.telescope.longitude_rad,mjd)
+                self.alts, self.azs = stupidFast_RaDec2AltAz(self.ra, self.dec,
+                                                             self.telescope.latitude_rad,
+                                                             self.telescope.longitude_rad, mjd)
 
         self.npts = self.ra.size
         self._initPoints()
@@ -243,8 +247,8 @@ class SkyModel(object):
             self.eclipLon = np.zeros(self.npts)
             self.eclipLat = np.zeros(self.npts)
 
-            for i,temp in enumerate(self.ra):
-                eclip = ephem.Ecliptic(ephem.Equatorial(self.ra[i],self.dec[i], epoch='2000'))
+            for i, temp in enumerate(self.ra):
+                eclip = ephem.Ecliptic(ephem.Equatorial(self.ra[i], self.dec[i], epoch='2000'))
                 self.eclipLon[i] += eclip.lon
                 self.eclipLat[i] += eclip.lat
             # Subtract off the sun ecliptic longitude
@@ -255,14 +259,12 @@ class SkyModel(object):
 
         self.paramsSet = True
 
-        self.mask = np.where((self.airmass > self.airmassLimit) | (self.airmass < 1. ) )[0]
-        self.goodPix = np.where((self.airmass <= self.airmassLimit) | (self.airmass >= 1. ) )[0]
+        self.mask = np.where((self.airmass > self.airmassLimit) | (self.airmass < 1.))[0]
+        self.goodPix = np.where((self.airmass <= self.airmassLimit) | (self.airmass >= 1.))[0]
         # Interpolate the templates to the set paramters
         self.interpSky()
 
-
-
-    def setParams(self, airmass=1.,azs=90., alts=None, moonPhase=31.67, moonAlt=45.,
+    def setParams(self, airmass=1., azs=90., alts=None, moonPhase=31.67, moonAlt=45.,
                   moonAz=0., sunAlt=-12., sunAz=0., sunEclipLon=0.,
                   eclipLon=135., eclipLat=90., degrees=True, solarFlux=130.):
         """
@@ -278,7 +280,7 @@ class SkyModel(object):
         else:
             convertFunc = justReturn
 
-        self.solarFlux=solarFlux
+        self.solarFlux = solarFlux
         self.sunAlt = convertFunc(sunAlt)
         self.moonPhase = moonPhase
         self.moonAlt = convertFunc(moonAlt)
@@ -322,16 +324,14 @@ class SkyModel(object):
 
         self.paramsSet = True
 
-        self.mask = np.where((self.airmass > self.airmassLimit) | (self.airmass < 1. ) )[0]
-        self.goodPix = np.where((self.airmass <= self.airmassLimit) | (self.airmass >= 1. ) )[0]
+        self.mask = np.where((self.airmass > self.airmassLimit) | (self.airmass < 1.))[0]
+        self.goodPix = np.where((self.airmass <= self.airmassLimit) | (self.airmass >= 1.))[0]
         # Interpolate the templates to the set paramters
         self.interpSky()
 
-
-
     def interpSky(self):
         """
-        Interpolate the template spectra to the set RA,Dec and MJD.
+        Interpolate the template spectra to the set RA, Dec and MJD.
 
         the results are stored as attributes of the class:
         .wave = the wavelength in nm
@@ -341,13 +341,13 @@ class SkyModel(object):
         if not self.paramsSet:
             raise ValueError('No parameters have been set. Must run setRaDecMjd or setParams before running interpSky.')
 
-        # set up array to hold the resulting spectra for each ra,dec point.
+        # set up array to hold the resulting spectra for each ra, dec point.
         self.spec = np.zeros((self.npts, self.npix), dtype=float)
 
         # Rebuild the components dict so things can be turned on/off
-        self.components = {'moon':self.moon, 'lowerAtm':self.lowerAtm, 'twilight':self.twilight,
-                           'upperAtm':self.upperAtm, 'airglow':self.airglow,'zodiacal':self.zodiacal,
-                           'scatteredStar':self.scatteredStar, 'mergedSpec':self.mergedSpec}
+        self.components = {'moon': self.moon, 'lowerAtm': self.lowerAtm, 'twilight': self.twilight,
+                           'upperAtm': self.upperAtm, 'airglow': self.airglow, 'zodiacal': self.zodiacal,
+                           'scatteredStar': self.scatteredStar, 'mergedSpec': self.mergedSpec}
 
         # Loop over each component and add it to the result.
         mask = np.ones(self.npts)
@@ -358,20 +358,20 @@ class SkyModel(object):
                 if np.max(result['spec']) > 0:
                     mask[np.where(np.sum(result['spec'], axis=1) == 0)] = 0
                 self.spec[self.goodPix] += result['spec']
-                if not hasattr(self,'wave'):
+                if not hasattr(self, 'wave'):
                     self.wave = result['wave']
                 else:
-                    if not np.allclose(result['wave'], self.wave, rtol= 1e-5,atol=1e-5):
+                    if not np.allclose(result['wave'], self.wave, rtol=1e-5, atol=1e-5):
                         warnings.warn('Wavelength arrays of components do not match.')
-        self.spec[np.where(mask == 0),:] = 0
-        self.spec[self.mask,:] = np.nan
+        self.spec[np.where(mask == 0), :] = 0
+        self.spec[self.mask, :] = np.nan
 
     def returnWaveSpec(self):
         """
         Return the wavelength and spectra
         """
         if self.mags:
-            raise ValueError('skyModel has been set to interpolate magnitudes. Initialize object with mags=False')
+            raise ValueError('SkyModel set to interpolate magnitudes. Initialize object with mags=False')
         # Mask out high airmass points
         # self.spec[self.mask] *= 0
         return self.wave, self.spec
@@ -391,11 +391,11 @@ class SkyModel(object):
             isThrough = np.where(bandpass.sb > 0)
             minWave = bandpass.wavelen[isThrough].min()
             maxWave = bandpass.wavelen[isThrough].max()
-            inBand = np.where( (self.wave >= minWave) & (self.wave <= maxWave))
+            inBand = np.where((self.wave >= minWave) & (self.wave <= maxWave))
             for i, ra in enumerate(self.ra):
                 # Check that there is flux in the band, otherwise calcMag fails
-                if np.max(self.spec[i,inBand]) > 0:
-                    tempSed.setSED(self.wave, flambda=self.spec[i,:])
+                if np.max(self.spec[i, inBand]) > 0:
+                    tempSed.setSED(self.wave, flambda=self.spec[i, :])
                     mags[i] = tempSed.calcMag(bandpass)
 
         # Mask out high airmass
