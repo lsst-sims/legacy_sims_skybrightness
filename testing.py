37edd48518cad57a4a8b60f1f994add24eb2c2cd
Peter Yoachim <yoachim@uw.edu>
2017-03-01 12:50:16 -0800

    finsihed testing

--- testing.py
+++ testing.py
@@ -8,7 +8,36 @@ hpmap = np.zeros(hp.nside2npix(nside))
 lat, ra = hp.pix2ang(nside, np.arange(hpmap.size))
 dec = np.pi/2-lat
 
-mjd = 60291.35423611111
+#mjd = 60291.35423611111 -.2
+mjd = 49353.177645
 sm.setRaDecMjd(ra, dec, mjd)
 mags = sm.returnMags()
+mags
+
+sm = sb.SkyModel(twilight=True, zodiacal=False, moon=True,
+                 airglow=True, lowerAtm=False, upperAtm=False, scatteredStar=False,
+                 mergedSpec=True, mags=True)
+
+sm.setRaDecMjd(ra, dec, mjd)
+mags = sm.returnMags()
+mags
+
+
+# OK, moon does not appear to be constributing when it should be huge on the linux machine.
+# airglow and mergedSpec seem to be fine.
+# 
+
+
+# twilight check
+mjd = 49353.177645-.14
+sm = sb.SkyModel(twilight=True, zodiacal=False, moon=False,
+                 airglow=True, lowerAtm=False, upperAtm=False, scatteredStar=False,
+                 mergedSpec=True, mags=True)
+
+sm.setRaDecMjd(ra, dec, mjd)
+mags = sm.returnMags()
+mags
+
+# Twilight is fine.
+
 
