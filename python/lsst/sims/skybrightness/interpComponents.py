6c2388c44fc8d33ca5576967c3484e357af9f3a4
danielsf <scott.f.daniel@gmail.com>
2018-06-29 13:15:04 -0700

    replace !=0

--- python/lsst/sims/skybrightness/interpComponents.py
+++ python/lsst/sims/skybrightness/interpComponents.py
@@ -142,8 +142,9 @@ class BaseSingleInterp(object):
         # Take the log of the spectra in case we want to interp in log space.
         if not mags:
             self.logSpec = np.zeros(self.spec['spectra'].shape, dtype=float)
-            good = np.where(self.spec['spectra'] > 1.0e-10)
-            self.logSpec[good] = np.log10(self.spec['spectra'][good])
+            log_raw = np.log10(self.spec['spectra'])
+            good = np.where(np.isfinite(log_raw))
+            self.logSpec[good] = log_raw[good]
             self.specSize = self.spec['spectra'][0].size
         else:
             self.specSize = 0
