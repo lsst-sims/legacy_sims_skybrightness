3661becdd3a792407017a3aedce52e01e7bbe015
Peter Yoachim <yoachim@uw.edu>
2020-09-23 13:10:28 -0700

    tools look good

--- data/ESO_Spectra/eso_tools.py
+++ data/ESO_Spectra/eso_tools.py
@@ -1,8 +1,9 @@
 import numpy as np
-import glob
 from astropy.io import fits
 import os, subprocess
 from lsst.sims.photUtils import Sed, Bandpass
+import healpy as hp
+from lsst.sims.utils import angularSeparation
 
 ## Tools for calling and reading things from the ESO sky model.
 # Downloaded and installed from XXX.
@@ -183,25 +184,110 @@ def generate_loweratm(outDir=None):
 
 
 def merged_spec():
-    # XXX--merge all the airmass-only spectra together
-    pass
+    dataDir = os.getenv('SIMS_SKYBRIGHTNESS_DATA_DIR')
+    outDir = os.path.join(dataDir, 'ESO_Spectra/MergedSpec')
+
+    # A large number of the background components only depend on Airmass, so we can merge those together
+
+    npzs = ['LowerAtm/Spectra.npz',
+            'ScatteredStarLight/scatteredStarLight.npz',
+            'UpperAtm/Spectra.npz']
+    files = [os.path.join(dataDir, 'ESO_Spectra', npz) for npz in npzs]
+    temp = np.load(files[0])
+    wave = temp['wave'].copy()
+    spec = temp['spec'].copy()
+    spec['spectra'] = spec['spectra']*0.
+    spec['mags'] = spec['mags']*0.
+
+    for filename in files:
+        restored = np.load(filename)
+        spec['spectra'] += restored['spec']['spectra']
+        flux = 10.**(-0.4*(restored['spec']['mags']-np.log10(3631.)))
+        flux[np.where(restored['spec']['mags'] == 0.)] = 0.
+        spec['mags'] += flux
+
+    spec['mags'] = -2.5*np.log10(spec['mags'])+np.log10(3631.)
+
+    np.savez(os.path.join(outDir, 'mergedSpec.npz'), spec=spec, wave=wave, filterWave=temp['filterWave'])
+
 
 def generate_moon(outDir=None):
     if outDir is None:
         dataDir = os.getenv('SIMS_SKYBRIGHTNESS_DATA_DIR')
-        outDir = os.path.join(dataDir, 'ESO_Spectra/LowerAtm')
+        outDir = os.path.join(dataDir, 'ESO_Spectra/Moon')
+
+    nside = 4
+    hpids = np.arange(hp.nside2npix(nside))
+    lat, az = hp.pix2ang(nside, hpids)
+    alt = np.pi/2.-lat
+    airmass = 1./np.cos(np.pi/2.-alt)
+
+    # Only need low airmass and then 1/2 to sky
+    good = np.where((az >= 0) & (az <= np.pi) & (airmass <= 3.01) & (airmass >= 1.))
+    airmass = airmass[good]
+    alt = np.degrees(alt[good])
+    az = np.degrees(az[good])
+    hpids = hpids[good]
+
+    moonSunSeps = np.array([0.0, 15, 30, 45, 60, 75, 90, 105, 120, 135,
+                            150, 165, 180], dtype=float)
+    moonAlts = np.array([-15, 0, 15, 30, 45, 60, 75, 90], dtype=float)
+
+    specs = []
+    final_moonSunSep = []
+    final_hpid = []
+    final_moonAlt = []
+
+    for moonSunSep in moonSunSeps:
+        for moonAlt in moonAlts:
+            angDists = angularSeparation(0., moonAlt, az, alt)
+            for salt, saz, am, angDist, hpid in zip(alt, az, airmass, angDists, hpids):
+                write_config(alpha=moonSunSep, alt=salt, altmoon=moonAlt, rho=angDist, inc_moon='Y')
+                call_calcskymodel()
+                spec, wave, header = read_eso_output()
+                specs.append(spec)
+                final_moonSunSep.append(moonSunSep)
+                final_hpid.append(hpid)
+                final_moonAlt.append(moonAlt)
+
+    mags, filterwave = spec2mags(specs, wave)
+    nwave = wave.size
+    nspec = len(specs)
+
+    dtype = [('hpid', 'int'),
+             ('moonAltitude', 'float'),
+             ('moonSunSep', 'float'),
+             ('spectra', 'float', (nwave)), ('mags', 'float', (6))]
+    spectra = np.zeros(nspec, dtype=dtype)
+    spectra['hpid'] = final_hpid
+    spectra['moonAltitude'] = final_moonAlt
+    spectra['moonSunSep'] = final_moonSunSep
+    spectra['spectra'] = specs
+
+    spectra.sort(order=['moonSunSep', 'moonAltitude', 'hpid'])
+
+    nbreak = 5
+    indices = np.linspace(0, spectra.size, nbreak+1, dtype=int)
+
+    for i in np.arange(nbreak):
+        np.savez(os.path.join(outDir, 'moonSpectra_'+str(i)+'.npz'), wave=wave,
+                 spec=spectra[indices[i]:indices[i+1]], filterWave=filterwave)
+
+
+def generate_scatteredStar(outDir=None):
+    if outDir is None:
+        dataDir = os.getenv('SIMS_SKYBRIGHTNESS_DATA_DIR')
+        outDir = os.path.join(dataDir, 'ESO_Spectra/ScatteredStarLight')
 
     ams = np.array([1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.5, 3.0])
     specs = []
 
     alts = 90.-np.degrees(np.arccos(1./ams))
     airmasses = []
-    moonSunSeps = []
-    moonAltitudes = []
-    hpids = []
+    nightTimes = []
 
     for alt, am in zip(alts, ams):
-        write_config(alt=alt, inc_molec='Y')
+        write_config(alt=alt, inc_star='Y')
         call_calcskymodel()
         spec, wave, header = read_eso_output()
         specs.append(spec)
@@ -222,17 +308,114 @@ def generate_moon(outDir=None):
     spectra['spectra'] = specs
     spectra['mags'] = mags['mags']
 
-    spectra.sort(order=['moonSunSep', 'moonAltitude', 'hpid'])
+    spectra.sort(order=['airmass', 'nightTimes'])
 
-    nbreak = 5
+    np.savez(os.path.join(outDir, 'scatteredStarLight.npz'), wave=wave, spec=spectra, filterWave=filterwave)
+
+
+def generate_upperatm(outDir=None):
+    if outDir is None:
+        dataDir = os.getenv('SIMS_SKYBRIGHTNESS_DATA_DIR')
+        outDir = os.path.join(dataDir, 'ESO_Spectra/UpperAtm')
+
+    ams = np.array([1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.5, 3.0])
+    specs = []
+
+    alts = 90.-np.degrees(np.arccos(1./ams))
+    airmasses = []
+    nightTimes = []
+
+    for alt, am in zip(alts, ams):
+        write_config(alt=alt, inc_upper='Y')
+        call_calcskymodel()
+        spec, wave, header = read_eso_output()
+        specs.append(spec)
+        airmasses.append(am)
+        # Not doing a range of these this time. I suppose I could.
+        nightTimes.append(0)
+
+    mags, filterwave = spec2mags(specs, wave)
+    nwave = wave.size
+    nspec = len(specs)
+
+    dtype = [('airmass', 'float'),
+             ('nightTimes', 'float'),
+             ('spectra', 'float', (nwave)), ('mags', 'float', (6))]
+    spectra = np.zeros(nspec, dtype=dtype)
+    spectra['airmass'] = airmasses
+    spectra['nightTimes'] = nightTimes
+    spectra['spectra'] = specs
+    spectra['mags'] = mags['mags']
+
+    spectra.sort(order=['airmass', 'nightTimes'])
+
+    np.savez(os.path.join(outDir, 'Spectra.npz'), wave=wave, spec=spectra, filterWave=filterwave)
+
+
+def generate_zodi(outDir=None):
+    if outDir is None:
+        dataDir = os.getenv('SIMS_SKYBRIGHTNESS_DATA_DIR')
+        outDir = os.path.join(dataDir, 'ESO_Spectra/Zodiacal')
+
+    ams = np.array([1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.5, 3.0])
+    specs = []
+    final_hpid = []
+    nside = 4
+    hpids = np.arange(hp.nside2npix(nside))
+    theta, phi = hp.pix2ang(nside, hpids)
+    lon = np.degrees(phi)
+    lon[np.where(lon > 180.)] = lon[np.where(lon > 180.)]-360.
+    lat = np.degrees(theta)-90.
+
+    alts = 90.-np.degrees(np.arccos(1./ams))
+    airmasses = []
+
+    for la, lo, hpi in zip(lat, lon, hpids):
+        for alt, am in zip(alts, ams):
+            write_config(alt=alt, lon_ecl=lo, lat_ecl=la, inc_zodi='Y')
+            call_calcskymodel()
+            spec, wave, header = read_eso_output()
+            specs.append(spec)
+            airmasses.append(am)
+            final_hpid.append(hpi)
+
+    mags, filterwave = spec2mags(specs, wave)
+    nwave = wave.size
+    nspec = len(specs)
+
+    dtype = [('airmass', 'float'),
+             ('hpid', 'int'),
+             ('spectra', 'float', (nwave)), ('mags', 'float', (6))]
+    spectra = np.zeros(nspec, dtype=dtype)
+    spectra['airmass'] = airmasses
+    spectra['hpid'] = final_hpid
+    spectra['spectra'] = specs
+    spectra['mags'] = mags['mags']
+
+    spectra.sort(order=['airmass', 'hpid'])
+
+    # span this over multiple files to store in github
+    nbreak = 3
     indices = np.linspace(0, spectra.size, nbreak+1, dtype=int)
 
     for i in np.arange(nbreak):
-        break_index = np.int(np.floor(nrec/nbreak))
-        np.savez(os.path.join(outDir, 'moonSpectra_'+str(i)+'.npz'), wave=wave,
+        np.savez(os.path.join(outDir, 'zodiacalSpectra_'+str(i)+'.npz'), wave=wave,
                  spec=spectra[indices[i]:indices[i+1]], filterWave=filterwave)
 
 
+def recalc_mags():
+    # XXX--todo, make a function that loads up all the spectra and recalculates the magnitudes
+    pass
+
+
+if __name__ == '__main__':
+    generate_airglow()
+    generate_loweratm()
+    generate_moon()
+    generate_scatteredStar()
+    generate_upperatm()
+    generate_zodi()
+    merged_spec()
 
 
 
